b17cc64a81d24ba6a34d1de39f0a60c65d79ca0f
giuli <neoisgodlike@hotmail.com>
2017-03-20 18:46:09 +0100

    update with network properties

--- clevercloudhelloworld/src/net/giuli/clevercloud/HelloWorldServlet.java
+++ clevercloudhelloworld/src/net/giuli/clevercloud/HelloWorldServlet.java
@@ -2,6 +2,14 @@ package net.giuli.clevercloud;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
@@ -41,6 +49,10 @@ public class HelloWorldServlet extends HttpServlet {
 		for(Map.Entry<Object,Object>  entry : _properties.entrySet()){
 			writer.println("<p>" + entry.getKey() + " = "  + entry.getValue() + "</p>");
 		}
+		writer.println("	<h1>Network properties:</h1>");
+		for(Map.Entry<String,Object>  entry : collectNetworkProperties().entrySet()){
+			writer.println("<p>" + entry.getKey() + " = "  + entry.getValue() + "</p>");
+		}
 		
 		writer.println("<body>");
 		writer.println("</html>");
@@ -55,5 +67,28 @@ public class HelloWorldServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	
+	private Map<String,Object> collectNetworkProperties() throws SocketException {
+		
+		Map<String,Object> result = new HashMap<String,Object>();
+		
+		Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+		
+		ArrayList<NetworkInterface> list = Collections.list(nets);
+		for (int i = 0; i < list.size(); i++) {
+			NetworkInterface netint = list.get(i);
+			result.put("Name " + i, netint.getName());
+			Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+			String addresses = "";
+			for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+				if(inetAddress instanceof Inet4Address) {
+					addresses += inetAddress.toString() + " ";
+				}
+			}
+			result.put("InetAddresses " + i, addresses);
+		}
+		
+		return result;
+}
 
 }
